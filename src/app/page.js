de05f73442bd8e069a3fedfb50f9664d4f0ba15f
Shaheer2145 <shaheer.khan@z2climited.com>
2026-02-04 15:15:55 +0500

    kaam hogya ha

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,12 @@
-import Link from 'next/link'; /* trigger rebuild */
+
 import Header from './components/Header/Header';
-import Hero from './components/Hero/Hero';
-import AboutUs from './components/About/AboutUs';
-import MedicalDepartments from './components/Medical/MedicalDepartments';
-import Services247 from './components/Services/Services247';
-import NewsRoom from './components/News/NewsRoom';
-import FAQ from './components/FAQ/FAQ';
-import Footer from './components/Footer/Footer';
 import styles from './page.module.css';
 import AboutDuplicate from './components/About/AboutDuplicate.js';
 import MedicalDepartmentsDuplicate from './components/Medical/MedicalDepartmentsDuplicate';
 import ServicesDuplicate from './components/Services/ServicesDuplicate';
 import HeroDuplicate from './components/Hero/HeroDuplicate';
 import NewsDuplicate from './components/News/NewsDuplicate';
-import HeaderDuplicate from "./components/Duplicate/HeaderDuplicate";
+
 import FooterDuplicate from './components/Footer/FooterDuplicate';
 
 
