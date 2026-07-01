bb527251e252d759c8821c10187f993a16f97f61
Shaheer2145 <shaheer.khan@z2climited.com>
2026-07-01 12:23:31 +0500

    update success story and stats

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,47 +1,52 @@
 
-// export const dynamic = 'force-dynamic';
-// import AboutContent from "../components/AboutPage/AboutContent";
-
-
-// async function getPageData() {
-//     const baseUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000';
-//     const res = await fetch(`${baseUrl}/api/pages/69a0035c7668294512a0c277?depth=3&draft=false&locale=undefined&trash=false`, {
-//         next: { revalidate: 60 }
-//     });
-//     if (!res.ok) {
-//         throw new Error('Failed to fetch data')
-//     }
-//     const data = await res.json();
-//     return data;
-// }
-
-// export default async function AboutPage() {
-//     const data = await getPageData();
-//     if (!data) return <div>Error loading page content.</div>;
-
-//     return (
-//         <AboutContent data={data} />
-//     );
-// }
+export const dynamic = 'force-dynamic';
 import AboutContent from "../components/AboutPage/AboutContent";
-import { getPayload } from 'payload';
-import config from '@/payload.config'; 
 
-export const dynamic = 'force-dynamic'; 
+
+async function getPageData() {
+    const baseUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000';
+    const res = await fetch(`${baseUrl}/api/pages/69a0035c7668294512a0c277?depth=3&draft=false&locale=undefined&trash=false`, {
+        next: { revalidate: 60 }
+    });
+    if (!res.ok) {
+        throw new Error('Failed to fetch data')
+    }
+    const data = await res.json();
+    return data;
+}
 
 export default async function AboutPage() {
-    // Direct Database Query (No localhost network dependency)
-    const payload = await getPayload({ config });
-    
-    const data = await payload.findByID({
-        collection: 'pages',
-        id: '69a0035c7668294512a0c277',
-        depth: 3,
-    });
-
+    const data = await getPageData();
     if (!data) return <div>Error loading page content.</div>;
 
     return (
         <AboutContent data={data} />
     );
 }
+
+
+
+
+
+// import AboutContent from "../components/AboutPage/AboutContent";
+// import { getPayload } from 'payload';
+// import config from '@/payload.config'; 
+
+// export const dynamic = 'force-dynamic'; // Backup safely
+
+// export default async function AboutPage() {
+//     // Direct Database Query (No localhost network dependency)
+//     const payload = await getPayload({ config });
+    
+//     const data = await payload.findByID({
+//         collection: 'pages',
+//         id: '69a0035c7668294512a0c277',
+//         depth: 3,
+//     });
+
+//     if (!data) return <div>Error loading page content.</div>;
+
+//     return (
+//         <AboutContent data={data} />
+//     );
+// }
